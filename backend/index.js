fed59f81179ba7a755ac54caa5bae12dd26fd50e
Pratham-Panthari <142975767+Pratham-Panthari@users.noreply.github.com>
2023-08-23 23:04:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const roomRoute = require('./routes/roomRoute')
 const cityRoute = require('./routes/cityRoute')
 const reservationRoute = require('./routes/reservationRoute')
 const TestimonialRoute = require('./routes/testimonialRoute')
-const path = require('path')
+
 
 run()
 
@@ -20,7 +20,7 @@ app.use(cors())
 
 app.use(express.json())
 
-app.use(express.static(path.join(__dirname, '../client/dist')))
+
 
 app.use('/api/v1/auth', authRoute)
 app.use('/api/v1/user', userRoute)
@@ -31,9 +31,7 @@ app.use('/api/v1/testimonials', TestimonialRoute)
 app.use('/api/v1/reservation', reservationRoute)
 
 
-app.use('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
-})
+
 
 const PORT = process.env.PORT 
 
